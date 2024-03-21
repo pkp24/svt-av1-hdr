5360ced0851c06b2a0851e1c4e4e1ecd16a67549
Julio Barba <juliobbv@gmail.com>
2024-03-21 03:01:11 -0400

    Implement extended CRF support
    fix: extended CRF func fix

--- Source/API/EbSvtAv1Enc.h
+++ Source/API/EbSvtAv1Enc.h
@@ -951,6 +951,13 @@ typedef struct EbSvtAv1EncConfiguration {
      * Default is false.
      */
     bool rtc;
+
+    /* @brief Q index for extended CRF support
+     * Value is internally determined by CRF parameter value
+     * Default is 0 if CRF is an integer
+     */
+    uint8_t extended_crf_qindex_offset;
+
     // clang-format off
     /*Add 128 Byte Padding to Struct to avoid changing the size of the public configuration struct*/
     uint8_t padding[128 - (sizeof(uint8_t) * 2)
